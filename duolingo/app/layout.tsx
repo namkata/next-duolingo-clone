a8d3da61d777baf91d4bfc626e130588e24dff09
Nam Tran <t.nam11121998@gmail.com>
2024-04-02 18:07:58 +0700

    Add Exit Modal

--- duolingo/app/layout.tsx
+++ duolingo/app/layout.tsx
@@ -3,6 +3,7 @@ import { JetBrains_Mono } from "next/font/google";
 import { ClerkProvider } from '@clerk/nextjs';
 import "./globals.css";
 import { Toaster } from "@/components/ui/sonner";
+import { ExitModal } from "@/components/modals/exit-modal";
 
 const font = JetBrains_Mono({ subsets: ["latin"] });
 
@@ -20,6 +21,7 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <Toaster />
+        <ExitModal></ExitModal>
         <body className={font.className}>{children}</body>
       </html>
     </ClerkProvider>
