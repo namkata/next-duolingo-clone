599888c62d70b705a8f947047085854a13d7efc0
Nam Tran <t.nam11121998@gmail.com>
2024-04-09 14:54:02 +0700

    Add Challenge Footer

--- duolingo/app/lesson/challenge.tsx
+++ duolingo/app/lesson/challenge.tsx
@@ -1,31 +1,31 @@
-import { challengeOptions, challenges } from "@/db/schema";
-import { cn } from "@/lib/utils";
-import { Card } from "./card";
+import { challengeOptions, challenges } from '@/db/schema';
+import { cn } from '@/lib/utils';
+import { Card } from './card';
 
 type Props = {
   options: (typeof challengeOptions.$inferSelect)[];
   onSelect: (id: number) => void;
-  status: "correct" | "wrong" | "none";
-  selectedOptions?: number;
+  status: 'correct' | 'wrong' | 'none';
+  selectedOption?: number;
   disabled?: boolean;
-  type: (typeof challenges.$inferSelect)["type"];
+  type: (typeof challenges.$inferSelect)['type'];
 };
 
 export const Challenge = ({
   options,
   onSelect,
   status,
-  selectedOptions,
+  selectedOption,
   disabled,
   type,
 }: Props) => {
   return (
     <div
       className={cn(
-        "grid gap-2",
-        type === "ASSIST" && "grip-cols-1",
-        type === "SELECT" &&
-          "grip-cols-2 lg:grid-cols-[repeat(auto-fit,minmax(0,1fr))]"
+        'grid gap-2',
+        type === 'ASSIST' && 'grip-cols-1',
+        type === 'SELECT' &&
+          'grip-cols-2 lg:grid-cols-[repeat(auto-fit,minmax(0,1fr))]',
       )}
     >
       {options.map((option, i) => (
@@ -35,7 +35,7 @@ export const Challenge = ({
           text={option.text}
           imageSrc={option.imageSrc}
           shortcut={`${i + 1}`}
-          selected={selectedOptions === option.id}
+          selected={selectedOption === option.id}
           onClick={() => onSelect(option.id)}
           status={status}
           audioSrc={option.audioSrc}
--- duolingo/app/lesson/quiz.tsx
+++ duolingo/app/lesson/quiz.tsx
@@ -1,10 +1,11 @@
-"use client";
+'use client';
 
-import { challengeOptions, challenges } from "@/db/schema";
-import { useState } from "react";
-import { Header } from "./header";
-import { QuestionBubble } from "./question_bubble";
-import { Challenge } from "./challenge";
+import { challengeOptions, challenges } from '@/db/schema';
+import { useState } from 'react';
+import { Header } from './header';
+import { QuestionBubble } from './question_bubble';
+import { Challenge } from './challenge';
+import { Footer } from './footer';
 
 type Props = {
   initialPercentage: number;
@@ -29,17 +30,25 @@ export const Quiz = ({
   const [challenges] = useState(initialLessonChallenges);
   const [activeIndex, setActiveIndex] = useState(() => {
     const uncompletedIndex = challenges.findIndex(
-      (challenge) => !challenge.completed
+      (challenge) => !challenge.completed,
     );
     return uncompletedIndex === -1 ? 0 : uncompletedIndex;
   });
+  const [selectedOption, setSelectedOption] = useState<number>();
+  const [status, setStatus] = useState<'correct' | 'wrong' | 'none'>('none');
 
   const challenge = challenges[activeIndex];
   const options = challenge?.challengeOptions ?? [];
 
+  const onSelect = (id: number) => {
+    if (status != 'none') return;
+
+    setSelectedOption(id);
+  };
+
   const title =
-    challenge.type === "ASSIST"
-      ? "Select the correct meaning"
+    challenge.type === 'ASSIST'
+      ? 'Select the correct meaning'
       : challenge.question;
 
   return (
@@ -56,14 +65,14 @@ export const Quiz = ({
               {title}
             </h1>
             <div>
-              {challenge.type === "SELECT" && (
+              {challenge.type === 'SELECT' && (
                 <QuestionBubble question={challenge.question} />
               )}
               <Challenge
                 options={options}
-                onSelect={() => {}}
-                status="correct"
-                selectedOptions={undefined}
+                onSelect={onSelect}
+                status={status}
+                selectedOption={selectedOption}
                 disabled={false}
                 type={challenge.type}
               />
@@ -71,6 +80,13 @@ export const Quiz = ({
           </div>
         </div>
       </div>
+      <Footer
+        // disabled={pending || !selectedOption}
+        disabled={!selectedOption}
+        status={status}
+        onCheck={() => {}}
+        // onCheck={onContinue}
+      />
     </>
   );
 };
